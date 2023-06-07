cb0c1aa58c25ef2e954318e0215922f2f667170f
Pedram_m <mirelmipedram@gmail.com>
2023-06-07 18:48:29 +0330

    added some initial exceptions

---
+++ src/main/java/se/survivor/net/exceptions/InvalidIdException.java
@@ -0,0 +1,8 @@
+package se.survivor.net.exceptions;
+
+public class InvalidIdException extends RuntimeException {
+
+    public InvalidIdException(String message) {
+        super(message);
+    }
+}
