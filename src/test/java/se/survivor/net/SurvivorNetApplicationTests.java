7f5b3d76d25a0d3089b0e9c41e2a799f83422925
Pedram_m <mirelmipedram@gmail.com>
2023-06-07 18:50:04 +0330

    initial project files

---
+++ src/test/java/se/survivor/net/SurvivorNetApplicationTests.java
@@ -0,0 +1,13 @@
+package se.survivor.net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurvivorNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
