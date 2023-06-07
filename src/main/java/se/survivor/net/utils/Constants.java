4157314347a8ed8d80bf26cb423eba6912043ff2
Pedram_m <mirelmipedram@gmail.com>
2023-06-07 18:49:10 +0330

    added some initial utils and configurations

---
+++ src/main/java/se/survivor/net/utils/Constants.java
@@ -0,0 +1,22 @@
+package se.survivor.net.utils;
+
+public class Constants {
+    public static final String AUTHORIZATION = "authorization";
+
+    public static final String USER_ID = "userId";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String EMAIL = "email";
+    public static final String NAME = "name";
+
+    public static final String BIRTHDATE = "birthDate";
+    public static final String STATUS = "status";
+    public static final String SUCCESS = "success";
+    public static final String COUNT = "count";
+
+
+    public static final String COMMENT_ID = "commentId";
+    public static final String VOTE = "vote";
+    public static final String LIKES = "likes";
+    public static final String DISLIKES = "dislikes";
+}
