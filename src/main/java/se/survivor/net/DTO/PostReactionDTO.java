3b24d3cd8610bc5f513b48cd790e619a5a5be5f6
Pedram_m <mirelmipedram@gmail.com>
2023-07-06 12:45:27 +0330

    Implemented Post Reaction feature

---
+++ src/main/java/se/survivor/net/DTO/PostReactionDTO.java
@@ -0,0 +1,35 @@
+package se.survivor.net.DTO;
+
+
+import org.hibernate.Hibernate;
+import se.survivor.net.models.PostReaction;
+
+public class PostReactionDTO {
+    private UserDTO user;
+    private long postId;
+    private int reactionType;
+
+    public PostReactionDTO(UserDTO user, long postId, int reactionType) {
+        this.user = user;
+        this.postId = postId;
+        this.reactionType = reactionType;
+    }
+
+    public PostReactionDTO(PostReaction postReaction) {
+        this.user = new UserDTO(postReaction.getUser());
+        this.postId = postReaction.getPost().getPostId();
+        this.reactionType = postReaction.getReactionType();
+    }
+
+    public UserDTO getUser() {
+        return user;
+    }
+
+    public long getPostId() {
+        return postId;
+    }
+
+    public int getReactionType() {
+        return reactionType;
+    }
+}
--- src/main/java/se/survivor/net/controllers/PostController.java
+++ src/main/java/se/survivor/net/controllers/PostController.java
@@ -16,7 +16,7 @@ import java.util.Objects;
 import static se.survivor.net.utils.Constants.*;
 
 @RestController
-@RequestMapping("/api/posts/")
+@RequestMapping("/api/posts/") // www.survivor-net.com/api/posts/******
 public class PostController {
 
     private DbService dbService;
@@ -31,6 +31,7 @@ public class PostController {
         this.commentService = commentService;
     }
 
+
     @GetMapping("home")
     public List<PostDTO> getHomePosts(
             @RequestHeader(AUTHORIZATION) String jwtToken,
@@ -44,6 +45,7 @@ public class PostController {
         return postService.getPostDTOWithComments(postId);
     }
 
+    // www.survivor-net.com/api/posts/
     @PostMapping("")
     public Map<String, String > addPost(
             @RequestHeader(AUTHORIZATION) String jwtToken,
